66881d2038b831a773d47c21fe92a65e268720e0
Vladimir Grichina <vgrichina@gmail.com>
2019-03-19 02:00:47 -0700

    Demo compiler hanging

--- tests/near-bindgen/test.sh
+++ tests/near-bindgen/test.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 BASEDIR=../..
 $BASEDIR/bin/asc model.ts --nearFile model_near.ts
-$BASEDIR/bin/asc main.ts --nearFile main_near.ts
-$BASEDIR/bin/asc main_near.ts -o main_near.wat
-$BASEDIR/bin/asc test.ts -o test.wasm --textFile test.wat
+$BASEDIR/bin/asc main.ts -o test.wasm
+#$BASEDIR/bin/asc main_near.ts -o main_near.wat
+#$BASEDIR/bin/asc test.ts -o test.wasm --textFile test.wat
 prettier --parser typescript --write main_near.ts
 prettier --parser typescript --write model_near.ts
 diff -U 5 main_near.ts.expected main_near.ts
