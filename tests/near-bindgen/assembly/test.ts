3c2f5095e2425d4000c6e6944c84c637a9c1be0c
Willem Wyndham <willem.neal@gmail.com>
2019-08-23 16:03:58 -0400

    Now passes compiler tests

--- tests/near-bindgen/assembly/test.ts
+++ tests/near-bindgen/assembly/test.ts
@@ -1,17 +1,11 @@
 import * as main from "./main";
-// import * as model from "./model";
 import { base64, logging } from "near-runtime-ts";
 import { FooBar } from "./model";
 import { u128 } from "bignum";
 
-//@ts-ignore
-// @external("env", "log_utf8")
-// declare function log(str: string): void;
-
 export function runTest(): void {
     logging.log("starting test");
     let original = new FooBar();
-    logging.log("u64 = " + original.u64Val.toString())
     original.u32Arr = [42, 11];
     original.foo = 321;
     original.bar = 123;
@@ -25,22 +19,19 @@ export function runTest(): void {
     original.uint8arrays[1] = base64.decode("aGVsbG8sIHdvcmxkIQ==");
     original.u64Arr =  [10000000000, 100000000000];
     let encoder = original.encode();
-    logging.log(encoder.toString());
-    // logging.log("hasntoehuasoetnuhasoentuhaseontuh")
+    logging.log("Before: " + encoder.toString());
     //@ts-ignore
     let encoded = encoder.serialize();
     //@ts-ignore
     let decoded: FooBar = decode<FooBar>(encoded);
-    logging.log(decoded.toJSON());
+    logging.log("After: "+ decoded.toJSON());
     assert(original.foo == decoded.foo);
     assert(original.bar == decoded.bar);
-    // assert(original.u32Arr == decoded.u32Arr);
     assert(base64.encode(original.uint8array) == base64.encode(decoded.uint8array));
     assert(base64.encode(original.uint8arrays[0]) == base64.encode(decoded.uint8arrays[0]));
     assert(original.arr[0][0] == "Hello");
     assert(original.u64Arr[0] == decoded.u64Arr[0]);
-    // logging.log(testArrays.toString());
-    // assert(original.uint8arrays == decoded.uint8arrays);
+    logging.log("Test Passed");
 }
 
 export function convertFoobars(): void {
